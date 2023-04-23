b02652074d75554d0395d37097a4f1e8ada123b1
Kimo <kimo@ya.code>
2023-04-23 17:21:06 +0300

    rewrited hash calculating

--- signer/signer.go
+++ signer/signer.go
@@ -19,19 +19,48 @@ func ExecutePipeline() {
 
 func SingleHash(data int) string {
 	stData := strconv.Itoa(data)
-	crs32 := DataSignerCrc32(stData)
-	crs32md5 := DataSignerMd5(stData)
-	return crs32 + crs32md5
+	for i := 0; i < data; i++ {
+		crs32 := DataSignerCrc32(stData)
+		crs32md5 := DataSignerCrc32(DataSignerMd5(stData))
+		fmt.Println(stData + "SingleHash data: " + stData)
+		fmt.Println(stData + "SingleHash md5(data): " + DataSignerMd5(stData))
+		fmt.Println(stData + "SingleHash crc32(md5(data)): " + DataSignerCrc32(DataSignerMd5(stData)))
+		fmt.Println(stData + "SingleHash crc32(data): " + DataSignerCrc32(stData))
+		fmt.Println(stData + "SingleHash result: " + (crs32 + crs32md5))
+		return crs32 + crs32md5
+	}
+	return ""
 }
 
 func MultiHash(data int) string {
-	var result string
-	for i := 0; i < 6; i++ {
-		result += DataSignerCrc32(strconv.Itoa(i) + SingleHash(data))
+	step1 := SingleHash(data)
+	var strI string
+	for i := 0; i < data; i++ {
+		var result string
+		for i := 0; i < 6; i++ {
+			strI = strconv.Itoa(i)
+			result += DataSignerCrc32(strI + step1)
+			fmt.Println(step1 + " MultiHash: crc32(th+step1)) " +
+				strI + " " + DataSignerCrc32(strI+step1))
+		}
+		fmt.Println(step1 + " MultiHash result :  " + result)
+		return result
 	}
-	return result
+	return ""
 }
 
 func CombineResults(data int) {
-	fmt.Println(SingleHash(data) + "_" + MultiHash(data))
+	var tmp []string
+	var result string
+	tmp = append(tmp, MultiHash(data))
+	for i := range tmp {
+		result += strconv.Itoa(i) + "_"
+	}
+	s1 := result
+	if last := len(s1) - 1; last >= 0 && s1[last] == '_' {
+		s1 = s1[:last]
+	}
+	fmt.Println("s1:", s1)
+	fmt.Println("CombineResults: " + s1)
+
 }
